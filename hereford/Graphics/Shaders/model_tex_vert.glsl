e62dd6bc3932b6f8958accec2d698514a575f12a
Ligh7ninG <Lt1118@live.cn>
2024-08-17 00:44:32 -0700

    shader for skeletal anim

--- hereford/Graphics/Shaders/model_tex_vert.glsl
+++ hereford/Graphics/Shaders/model_tex_vert.glsl
@@ -2,6 +2,8 @@
 layout (location = 0) in vec3 inPos;
 layout (location = 1) in vec3 inNormal;
 layout (location = 2) in vec2 inTexCoords;
+layout (location = 5) in ivec4 inBoneIDs;
+layout (location = 6) in vec4 inWeights;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -10,12 +12,35 @@ uniform mat4 projection;
 out vec2 TexCoords;
 out vec3 WorldPos;
 out vec3 Normal;
+out mat4 sampleMat;
+
+const int MAX_BONES = 100;
+const int MAX_BONE_INFLUENCE = 4;
+uniform mat4 finalBonesMatrices[MAX_BONES];
+
 
 
 void main()
 {
+    vec4 totalPosition = vec4(0.0f);
+    for(int i=0;i<MAX_BONE_INFLUENCE;i++)
+    {
+        if(inBoneIDs[i] == -1)
+            continue;
+        if(inBoneIDs[i] >= MAX_BONES)
+        {
+            totalPosition = vec4(inPos, 1.0f);
+            break;
+        }
+        vec4 localPos = finalBonesMatrices[inBoneIDs[i]] * vec4(inPos, 1.0f);
+        totalPosition += localPos * inWeights[i];
+        vec3 localNormal = mat3(finalBonesMatrices[inBoneIDs[i]]) * inNormal;
+     }
+    sampleMat = finalBonesMatrices[inBoneIDs[0]];
     TexCoords = inTexCoords;    
-    WorldPos = vec3(model * vec4(inPos, 1.0f));
+    WorldPos = vec3(model * totalPosition);
     Normal = inNormal;
-    gl_Position = projection * view * model * vec4(inPos, 1.0);
+    gl_Position = projection * view * model * totalPosition;
+    //gl_Position = projection * view * model * vec4(inPos, 1.0f);
+
 }
