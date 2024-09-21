aa661af95957d572ef3210b5737ed9d668330167
Ligh7ninG <Lt1118@live.cn>
2024-09-20 22:57:02 -0700

    action & action component class

--- hereford/Gameplay/Action.h
+++ hereford/Gameplay/Action.h
@@ -1,5 +1,22 @@
 #pragma once
+#include "Util/GameplayTagContainer.h"
+
 class Action
 {
+public:
+	Action();
+	~Action();
+
+	bool IsRunning() const;
+	bool CanStart() const;
+	void StartAction();
+	void StopAction();
+
+
+protected:
+	GameplayTagContainer mGrantsTags;
+	GameplayTagContainer mBlockedTags;
+
+	float mTimeStarted;
 };
 
--- hereford/Gameplay/ActionComponent.h
+++ hereford/Gameplay/ActionComponent.h
@@ -1,5 +1,23 @@
 #pragma once
-class ActionComponent
+#include "Action.h"
+#include "Core/Component.h"
+#include "Util/GameplayTagContainer.h"
+#include <vector>
+
+
+class ActionComponent : public Component
 {
+public:
+	ActionComponent();
+	~ActionComponent();
+
+	void AddAction();
+	void RemoveAction();
+	bool HasAction();
+
+
+protected:
+	std::vector<Action> mActions;
+	GameplayTagContainer mActiveGameplayTags;
 };
 
