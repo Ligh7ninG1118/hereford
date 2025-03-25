19d3d19265e6f0c3f6644574219184800716a3a5
Ligh7ninG <Lt1118@live.cn>
2025-03-24 20:36:22 -0700

    ecs base classes

--- hereford/Core/Component.h
+++ hereford/Core/Component.h
@@ -1,40 +1,14 @@
 #pragma once
 
-#include "Util/Enums.h"
-#include <SDL2/SDL_stdinc.h>
-#include <vector>
-#include <memory>
 
-enum class EComponentState
-{
-	Enabled = 0,
-	Disabled,
-};
-
-class OldComponent
+class Component
 {
 public:
-	// Constructor
-	OldComponent(class Actor* owner, int updateOrder = 100);
-	// Destructor
-	virtual ~OldComponent();
-	// Update this component by delta time
-	virtual void Update(float deltaTime);
-	// Process input for this component (if needed)
-	virtual void ProcessInput(const std::vector<EInputState>& keyState, Uint32 mouseState, int mouseDeltaX, int mouseDeltaY);
+	Component();
+	virtual void Create();
+	virtual ~Component();
 
-	int GetUpdateOrder() const { return mUpdateOrder; }
-	class Actor* GetOwner() const { return mOwner; }
-	class Game* GetGame() const;
+private:
 
-	EComponentState GetState() const { return mState; }
-	void SetState(EComponentState state) { mState = state; }
-
-protected:
-	// Owning actor
-	class Actor* mOwner;
-	// Update order
-	int mUpdateOrder;
-	EComponentState mState;
 };
 
---
+++ hereford/Core/Entity.cpp
@@ -0,0 +1,10 @@
+#include "Entity.h"
+
+Entity::Entity(uint32_t inID)
+	: mEntityID(inID)
+{
+}
+
+Entity::~Entity()
+{
+}
